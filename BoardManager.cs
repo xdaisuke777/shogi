8afbdcaaaffc6eb460e991fddd36ed31c89f9bc3
xdaisuke777 <110794743+xdaisuke777@users.noreply.github.com>
2022-08-17 00:28:41 +0900

    ボードオブジェクトの情報
    座標位置、駒の移動可能位置フラグ

---
+++ BoardManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public int BoardX;
+    public int BoardY;
+
+    public bool PaintSelect = false;
+}
